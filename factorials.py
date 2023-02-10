2f2f72ca223543503395f67d63415944fe86c9c1
Christopher Cialone <ccialone0929@gmail.com>
2023-02-09 19:41:52 -0500

    here

---
+++ factorials.py
@@ -0,0 +1,15 @@
+def fact_while(n):
+    #4
+    total = 1 # 12
+    while n > 0:
+        total = total * n
+        n -= 1
+
+    print(total)
+
+#def fact_while(5):
+def fact_while(3):
+    # base case n ==1
+    # print total
+
+    # 
