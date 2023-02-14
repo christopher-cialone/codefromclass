354b7edcd4de8ec7fa66b2c722d0a4f2b1a7ae04
Christopher Cialone <ccialone0929@gmail.com>
2023-02-13 19:00:32 -0500

    sorting

--- factorials.py
+++ factorials.py
@@ -8,8 +8,13 @@ def fact_while(n):
     print(total)
 
 #def fact_while(5):
-def fact_while(3):
+def fact(n):
     # base case n ==1
-    # print total
+    # return n
+    if n == 1:
+        return n
+    else:
+        return n * fact(n - 1)
 
-    # 
+
+    # 4 * fact(3)
