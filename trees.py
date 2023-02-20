dcd894094427043b4aa47e16aac92fccb613bc47
Christopher Cialone <ccialone0929@gmail.com>
2023-02-20 18:55:00 -0500

    binary tree

--- trees.py
+++ trees.py
@@ -3,8 +3,21 @@ class ListNode:
         self.val = value
         self.next = None
 
-head = ListNode(10)
-second = ListNode(5)
+# head = ListNode(10)
+# second = ListNode(5)
 
 
-head.next = second
+# head.next = second
+
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+root =  BinaryTreeNode(10)
+root_left = BinaryTreeNode(11)
+root_right = BinaryTreeNode(-3)
+
+root.left = root.left
+root.right = root.right
