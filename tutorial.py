9238869100ebc3db15729317ef1103679df97e01
Christopher Cialone <ccialone0929@gmail.com>
2023-02-22 09:42:21 -0500

    tuples

--- tutorial.py
+++ tutorial.py
@@ -1,9 +1,20 @@
 # python data structures
-z = 'bug'
-print(sorted(z))
+# z = 'bug'
+# print(sorted(z))
 
-y = ['pig', 'cow', 'jerks']
-print(sorted(y))
+# y = ['pig', 'cow', 'jerks']
+# print(sorted(y))
 
-f = ('Kev', 'John,', 'Mike', 'Ned')
-print(sorted(f))
+# f = ('Kev', 'John,', 'Mike', 'Ned')
+# print(sorted(f))
+# print(sorted(f, key=lambda k: k[1])) # based on the second letter
+
+x = list()
+y = ['a', 25, 'fog', 8.43]
+tuple1 = (10,20)
+z = list(tuple1)
+
+a = [m for m in range(8)]
+print(a)
+b = [i**2 for i in range(10) if i>4]
+print(b)
