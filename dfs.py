250477b969c8759fe1f23563ee74e05f94b1fa53
Christopher Cialone <ccialone0929@gmail.com>
2023-02-23 19:29:19 -0500

    node

--- dfs.py
+++ dfs.py
@@ -1,10 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         
-        result = []
+        self.result = []
+
+        return self.result
+
+    def traverse(self, node):
+        # need out base case: if we see a node that is null, return
+        # assuming we are looking at a real node:
+        # pre-prder traversal: going all the way down and recording what we see on the way
+        # add the value we see to our results list
+        # recurse to the left (by calling traverse() agian)
+        # recurse to the right (by calling traverse() agian)
+
+        if node == None:
+            return
+        self.result.append(node.val)
+        traverse(node.left)
